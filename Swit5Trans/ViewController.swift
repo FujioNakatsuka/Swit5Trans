44b0eb2a4f3021adc8cd402c45a9a5cfd2401c61
Fujio Nakatsuka <com.nifty.cbm01799>
2020-02-03 10:51:21 +0900

    Initial Commit

---
+++ Swit5Trans/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swit5Trans
+//
+//  Created by 中塚富士雄 on 2020/02/03.
+//  Copyright © 2020 中塚富士雄. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
